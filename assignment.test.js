6ddff309dcad199ee12451acd530f8ed494733c0
berglind14 <berglind14@ru.is>
2017-10-23 11:16:22 +0000

    fyrsta

--- assignment.test.js
+++ assignment.test.js
@@ -1,7 +1,22 @@
 import * as add from './add';
 import { loop } from './index';
+import { throws } from './index';
 
 describe('index', () => {
+	describe('add', () => {
+		test('Should return correct sum for 1 + 1', () => {
+			expect(add.add(1, 1)).toBe(2);
+		});
+
+		test('Should return correct sum for null + 1', () => {
+			expect(add.add(null, 1)).toBe(1);
+		});
+
+		test('should return not a number when calling add with no parameters', () => {
+			expect(add.add()).tobeNan;
+		});
+	});
+
 	describe('loop', () => {
 		test('should call add 3 times', () => {
 			const addSpy = jest.spyOn(add, 'add');
