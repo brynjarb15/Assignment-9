b9046ec7d279228dce4c3ecaec3074cce63c36ca
brynjarb15 <brynjarb15@ru.is>
2017-10-23 08:35:59 +0000

    init

---
+++ assignment.test.js
@@ -0,0 +1,12 @@
+import * as add from './add';
+import { loop } from './index';
+
+describe('index', () => {
+	describe('loop', () => {
+		test('should call add 3 times', () => {
+			const addSpy = jest.spyOn(add, 'add');
+			loop(3);
+			expect(addSpy).toHaveBeenCalledTimes(3);
+		});
+	});
+});
